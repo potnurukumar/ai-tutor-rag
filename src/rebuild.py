c9c58bd7e0f505e437763647524df43f0fb5ffed
Potnuru Rakesh Kumar <rakerake774@gmail.com>
2026-07-05 15:44:16 +0530

    fix: rebuilt vectorstore successfully

---
+++ src/rebuild.py
@@ -0,0 +1,9 @@
+from loader import load_documents
+from splitter import split_documents
+from embeddings import create_vector_store
+
+documents = load_documents("../data")
+chunks = split_documents(documents)
+create_vector_store(chunks)
+
+print("Vectorstore rebuilt")
